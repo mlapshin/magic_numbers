56e0a04e3cb49bf40a1623873ecaa46fa3de6b35
Mike Lapshin <sotakone@sotakone.com>
2009-11-24 16:59:26 +0300

    updated README

--- test/magic_numbers_test.rb
+++ test/magic_numbers_test.rb
@@ -43,7 +43,7 @@ class MagicNumbersTest < ActiveSupport::TestCase
   end
 
   test "should correctly report magic numbers for specified values" do
-    assert_equal 3, Foo.magic_number_for(:roles, [:administrator, :user, 'aalsdkajajs'])
+    assert_equal 3, Foo.magic_number_for(:roles, [:administrator, :user, nil, 'aalsdkajajs'])
     assert_equal 0, Foo.magic_number_for(:roles, [])
     assert_equal 1, Foo.magic_number_for(:state, 'pending')
 
